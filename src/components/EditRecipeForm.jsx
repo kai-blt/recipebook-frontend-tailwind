037d7302a51f752c00d6ccb80ed3e4134fda74c4
kai-blt <blaitong@gmail.com>
2021-05-21 16:11:49 -0700

    tweaked form styling and margins

--- src/components/EditRecipeForm.jsx
+++ src/components/EditRecipeForm.jsx
@@ -80,100 +80,105 @@ const EditRecipeForm = () => {
   };
 
   return(
-    <div className="mt-24">    
-    <div className="w-full flex flex-row justify-end"><button onClick={cancel} className="button-close">X</button></div>      
-    <form>
-      <div className="mb-4">
-        <label className="label">Title</label>
-        <input
-          type="text"
-          name="name"
-          value={formValues.name}
-          onChange={handleChange}
-          className="input"
-        />
-        <label className="label">Type</label>
-        <input
-          type="text"
-          name="type"
-          value={formValues.type}
-          onChange={handleChange}
-          className="input"
-        />
-        <label className="label">Image URL</label>
-        <input
-          type="text"
-          name="imageURL"
-          value={formValues.imageURL}
-          onChange={handleChange}
-          className="input"
-        />   
-      </div>        
-      <div className="mb-4">
-        <h2 className="heading2">Ingredients</h2>
-        {formValues.ingredients.map((ing, index) => (
-          <>
-            <label className="label">Qty</label>
+    <div className="mt-20">    
+      <div className="form-container">
+        <div className="w-full flex flex-row justify-end"><button onClick={cancel} className="button-close">X</button></div>      
+        <form>
+          <h2 className="heading2">About</h2>
+          <div className="form-group">
+            <label className="label">Title</label>
             <input
               type="text"
-              name="quantity"
-              value={ing.quantity}
-              onChange={e => handleChange(e, index)}
-              className="input"
-            />     
-            <label className="label">Measure</label>
-            <input
-              type="text"
-              name="measurement"
-              value={ing.measurement}
-              onChange={e => handleChange(e, index)}
-              className="input"
-            />     
-            <label className="label">Ingredient</label>
-            <input
-              type="text"
-              name="ingredientname"
-              value={ing.name}
-              onChange={e => handleChange(e, index)}
-              className="input"
-            />     
-            <label className="label">Group</label>
-            <input
-              type="text"
-              name="group"
-              value={ing.ingredientgroup}
-              onChange={e => handleChange(e, index)}
+              name="name"
+              value={formValues.name}
+              onChange={handleChange}
               className="input"
             />
-            <div className="flex flex-row">
-              <button className="button-remove" onClick={e => delIngredient(e, index)}>-</button> 
-              <button className="button-add" onClick={e => addIngredient(e, index)}>+</button>    
-            </div>
-          </>
-        ))}      
-      </div>
-      <div className="mb-4">
-        <h2 className="heading2">Steps</h2>
-        {formValues.steps.map((stp, index) => (
-          <>
-            <label className="label">Step {stp.stepnumber}</label>
-            <textarea
+            <label className="label">Type</label>
+            <input
               type="text"
-              name="instructions"
-              value={stp.instructions}
-              onChange={e => handleChange(e, index)}
-              className="textarea"
+              name="type"
+              value={formValues.type}
+              onChange={handleChange}
+              className="input"
             />
-            <div className="flex flex-row">
-              <button className="button-remove" onClick={e => delStep(e, index)}>-</button> 
-              <button className="button-add" onClick={e => addStep(e, index)}>+</button>    
-            </div>
-          </>
-        ))}
+            <label className="label">Image URL</label>
+            <input
+              type="text"
+              name="imageURL"
+              value={formValues.imageURL}
+              onChange={handleChange}
+              className="input"
+            />   
+          </div>        
+          <div className="mb-4">
+            <h2 className="heading2">Ingredients</h2>
+            {formValues.ingredients.map((ing, index) => (
+              <div className="form-group">
+                <label className="label">Qty</label>
+                <input
+                  type="text"
+                  name="quantity"
+                  value={ing.quantity}
+                  onChange={e => handleChange(e, index)}
+                  className="input"
+                />     
+                <label className="label">Measure</label>
+                <input
+                  type="text"
+                  name="measurement"
+                  value={ing.measurement}
+                  onChange={e => handleChange(e, index)}
+                  className="input"
+                />     
+                <label className="label">Ingredient</label>
+                <input
+                  type="text"
+                  name="ingredientname"
+                  value={ing.name}
+                  onChange={e => handleChange(e, index)}
+                  className="input"
+                />     
+                <label className="label">Group</label>
+                <input
+                  type="text"
+                  name="group"
+                  value={ing.ingredientgroup}
+                  onChange={e => handleChange(e, index)}
+                  className="input"
+                />
+                <div className="flex flex-row justify-between">
+                  <button className="button-remove" onClick={e => delIngredient(e, index)}>-</button> 
+                  <button className="button-add" onClick={e => addIngredient(e, index)}>+</button>    
+                </div>
+              </div>
+            ))}      
+          </div>
+          <div className="mb-4">
+            <h2 className="heading2">Steps</h2>
+            {formValues.steps.map((stp, index) => (
+              <div className="mb-12 border-2 p-2 rounded-lg bg-gray-100">
+                <label className="label">Step {stp.stepnumber}</label>
+                <textarea
+                  type="text"
+                  name="instructions"
+                  value={stp.instructions}
+                  onChange={e => handleChange(e, index)}
+                  className="textarea"
+                />
+                <div className="flex flex-row justify-between">
+                  <button className="button-remove" onClick={e => delStep(e, index)}>-</button> 
+                  <button className="button-add" onClick={e => addStep(e, index)}>+</button>    
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-row justify-between">
+            <button className="button-cancel w-96 mr-1" onClick={deleteRecipe}>Delete</button>
+            <button className="button w-96 ml-1" onClick={e => submit(e)}>Submit</button>
+          </div>
+        </form>
       </div>
-      <button className="button" onClick={e => submit(e)}>Submit</button>
-      <button className="button-cancel" onClick={deleteRecipe}>Delete Recipe</button>
-    </form>
     </div>
   )
 }
